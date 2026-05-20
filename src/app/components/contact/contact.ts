07baad8268549c3a69282846c45ec9dd4467c5d5
unknown <luka.bradic01@gmail.com>
2026-05-20 14:17:05 +0200

    puuuush

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,9 +1,57 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.scss',
+  styleUrls: ['./contact.scss']
 })
-export class Contact {}
+export class Contact {
+
+  contactOpen = false;
+  loading = false;
+  success = false;
+
+  openContactModal() {
+    this.contactOpen = true;
+  }
+
+  closeContactModal() {
+    this.contactOpen = false;
+  }
+
+  sendMessage(form: any) {
+
+    if (form.invalid) return;
+
+    this.loading = true;
+
+    // Formspree submit trigger (manual POST)
+    fetch('https://formspree.io/f/xdajengp', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form.value)
+    }).then(() => {
+
+      this.loading = false;
+      this.contactOpen = false;
+      this.success = true;
+      form.reset();
+
+    }).catch(() => {
+
+      this.loading = false;
+      alert('Error sending message');
+
+    });
+  }
+
+  closeSuccess() {
+    this.success = false;
+  }
+}
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar {
+  menuOpen = false;
+
+toggleMenu() {
+  this.menuOpen = !this.menuOpen;
+}
+
+closeMenu() {
+  this.menuOpen = false;
+}
+}
